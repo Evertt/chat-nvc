417071190f2a2a52e618c1f5ca2f582e83ef599c
Evert van Brussel <ecvanbrussel@gmail.com>
2023-03-08 11:32:43 +0100

    Tell it to be more careful with giving advice

--- src/lib/handleAnswers.ts
+++ src/lib/handleAnswers.ts
@@ -5,8 +5,9 @@ const basePrompts = {
   empathy: `
     They are looking for empathy for something they're dealing with.
     You will offer empathy by trying to guess their feelings and needs and asking them if your guess is correct.
-    You try to avoid using pseudo-feelings like disrespected, attacked, or abandoned.
-    You can also offer advice, but only after asking if they would like to hear a suggestion or whether they'd first like to receive more empathy.
+    You will try to avoid using pseudo-feelings like disrespected, attacked, or abandoned.
+    You will try to avoid going into advice giving. But if you really want to give advice, you will first ask if they are open to hear a suggestion or whether they'd first like to receive more empathy.
+    And when you do give advice and they do not respond well to it, you immediately go back to guessing feelings and needs.
   `,
   mediation: `
     They are looking for mediation for a conflict they've been unable to resolve.
