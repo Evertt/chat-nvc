9eb286d6b9661c8deb936c5b09b3107a8576d4f7
Evert van Brussel <ecvanbrussel@gmail.com>
2023-03-13 14:49:05 +0100

    remove all the python code

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,32 +1,7 @@
-import TelegramServer from 'telegram-test-api'
-import { TELEGRAM_KEY } from '$env/static/private'
-import { describe, it, expect, beforeAll, afterAll } from "vitest"
+import { describe, it, expect } from 'vitest';
 
-describe('telegram', async () => {
-	let server: TelegramServer
-
-	beforeAll(async () => {
-		server = new TelegramServer()
-		await server.start()
-	})
-
-	afterAll(async () => {
-		await server.stop()
-	})
-
-	it('should start', async () => {
-		server.setWebhook({ url: 'http://0.0.0.0:8000/webhook' }, TELEGRAM_KEY)
-		const client = server.getClient(TELEGRAM_KEY, {
-			timeout: 10000,
-		})
-		const message = client.makeMessage('/start')
-		await client.sendMessage(message)
-		const updateResp = await client.getUpdates()
-		console.log(`Ok: ${updateResp.ok}, result:\n`, updateResp.result)
-		expect(updateResp.ok).to.be.true
-		updateResp.result.forEach(update => {
-			expect(update.message.text).to.equal("Hello, I'm a bot that's using Sanic and python-telegram-bot!")
-		})
-	})
-})
-
+describe('sum test', () => {
+	it('adds 1 + 2 to equal 3', () => {
+		expect(1 + 2).toBe(3);
+	});
+});
