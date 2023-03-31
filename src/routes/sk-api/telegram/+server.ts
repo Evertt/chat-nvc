0ed880ca499096bc5d1d78ee1dcb59dd479fdc9a
Evert van Brussel <ecvanbrussel@gmail.com>
2023-03-31 17:21:22 +0200

    Shit I should've awaited...

--- src/routes/sk-api/telegram/+server.ts
+++ src/routes/sk-api/telegram/+server.ts
@@ -5,9 +5,9 @@ import { TELEGRAM_KEY, TELEGRAM_WEBBOOK_TOKEN } from '$env/static/private'
 const bot = new Telegraf(TELEGRAM_KEY)
 
 export const GET: RequestHandler = async () => {
-	bot.telegram.deleteWebhook({
+	await bot.telegram.deleteWebhook({
     drop_pending_updates: true
   })
 
-	return new Response('webhook deleted...')
+	return new Response('awaited webhook deleted...')
 }
