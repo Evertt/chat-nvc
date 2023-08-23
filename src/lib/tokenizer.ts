24defee07ce7888757fb14473ef7106c6097417f
Evert van Brussel <ecvanbrussel@gmail.com>
2023-08-23 10:29:06 +0800

    Reverting back to old tokenizer for vercel

--- src/lib/tokenizer.ts
+++ src/lib/tokenizer.ts
@@ -1,61 +1,46 @@
-import { oneLine } from 'common-tags'
+import GPT3TokenizerImport from 'gpt3-tokenizer'
 import type {
 	CreateChatCompletionRequestMessage,
 	CompletionCreateParams
 } from 'openai/resources/chat'
-import tiktoken, { type TiktokenModel } from 'tiktoken'
 
-// This type allows arbitrary strings.
-type OpenAIChatModel = CompletionCreateParams['model']
-// Which I don't want, so that's what this Extract is for.
-type GPTModel = Extract<OpenAIChatModel, TiktokenModel>
+// I know this looks weird,
+// but this gets differently imported
+// in different environments...
+const GPT3Tokenizer: typeof GPT3TokenizerImport =
+	typeof GPT3TokenizerImport === 'function'
+		? GPT3TokenizerImport
+		: (GPT3TokenizerImport as any).default
 
-type ChatMessage = CreateChatCompletionRequestMessage // & { tokens: number }
-type Input = string | ChatMessage | ChatMessage[]
+const tokenizer = new GPT3Tokenizer({ type: 'gpt3' })
 
-const specialTokens = {
-	'<|im_start|>': 100264,
-	'<|im_end|>': 100265,
-	'<|im_sep|>': 100266
-} as const
-
-const getTokenizer = (model: GPTModel) => tiktoken.encoding_for_model(model, specialTokens)
-
-let tokenizer: tiktoken.Tiktoken | undefined = undefined
+type GPTModel = CompletionCreateParams['model']
+type ChatMessage = CreateChatCompletionRequestMessage
+type Input = ChatMessage | ChatMessage[]
 
 export const countTokens = (input?: Input, model: GPTModel = 'gpt-4'): number => {
 	if (!input) return 0
 
-	tokenizer ??= getTokenizer(model)
-
-	if (typeof input === 'string') return tokenizer.encode(input, Object.keys(specialTokens)).length
-
 	const isGpt3 = model.startsWith('gpt-3')
 
 	const msgSep = isGpt3 ? '\n' : ''
-	const roleSep = isGpt3 ? '\n' : '<|im_sep|>'
 
 	if (Array.isArray(input)) {
 		const msgSepsTokens = input.length * msgSep.length
-		const lastMsg = `<|im_start|>assistant${roleSep}`
-		const lastTokens = countTokens(lastMsg)
 
 		const tokenCount = input.reduce(
 			(count, message) => count + countTokens(message),
-			msgSepsTokens + lastTokens
+			msgSepsTokens + 3
 		)
-
-		tokenizer.free()
-		tokenizer = undefined
 
 		return tokenCount
 	}
 
 	const { name, role, content } = input
 
-	const msgString = oneLine`
-		<|im_start|>${name || role}${roleSep}${content}<|im_end|>
-	`
+	if (!content) return 0
 
-	return countTokens(msgString)
+	const tokens = tokenizer.encode((name || role) + content)
+
+	return tokens.text.length + 3
 }
